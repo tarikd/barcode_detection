942ac9db53531d0b5132ff1acc0f348d5e372439
Tarik Dadi <tarikdadi@me.com>
2014-12-01 03:52:59 +0100

    Corrections

--- detect_barcode.py
+++ detect_barcode.py
@@ -1,3 +1,6 @@
+# USAGE
+# python detect_barcode.py --image images/barcode_01.jpg
+
 # import the necessary packages
 import numpy as np
 import argparse
@@ -5,7 +8,7 @@ import cv2
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True, help="path to the image file")
+ap.add_argument("-i", "--image", required = True, help = "path to the image file")
 args = vars(ap.parse_args())
 
 # load the image and convert it to grayscale
@@ -14,8 +17,8 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 # compute the Scharr gradient magnitude representation of the images
 # in both the x and y direction
-gradX = cv2.Sobel(gray, ddepth=cv2.cv.CV32F, dx=1, dy=0, ksize=-1)
-gradY = cv2.Subel(gray, ddepth=cv2.cv.CV32F, dx=0, dy=1, ksize=-1)
+gradX = cv2.Sobel(gray, ddepth = cv2.cv.CV_32F, dx = 1, dy = 0, ksize = -1)
+gradY = cv2.Sobel(gray, ddepth = cv2.cv.CV_32F, dx = 0, dy = 1, ksize = -1)
 
 # subtract the y-gradient from the x-gradient
 gradient = cv2.subtract(gradX, gradY)
@@ -27,23 +30,24 @@ blurred = cv2.blur(gradient, (9, 9))
 
 # construct a closing kernel and apply it to the thresholded image
 kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (21, 7))
-closed = cv2.morphologyEX(thresh, cv2.MORPH_CLOSE, kernel)
+closed = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
 
 # perform a series of erosions and dilations
-closed = cv2.erode(closed, None, iterations=4)
-closed = cv2.dilate(closed, None, iterations=4)
+closed = cv2.erode(closed, None, iterations = 4)
+closed = cv2.dilate(closed, None, iterations = 4)
 
 # find the contours in the thresholded image, then sort the contours
 # by their area, keeping only the largest one
 (cnts, _) = cv2.findContours(closed.copy(), cv2.RETR_EXTERNAL,
-                             cv2.CHAIN_APPROX_SIMPLE)
-c = sorted(cnts, key=cv2.contourArea, reverse=True)[0]
+	cv2.CHAIN_APPROX_SIMPLE)
+c = sorted(cnts, key = cv2.contourArea, reverse = True)[0]
 
 # compute the rotated bounding box of the largest contour
 rect = cv2.minAreaRect(c)
 box = np.int0(cv2.cv.BoxPoints(rect))
 
-# draw a bounding box arounded the detexted barcode and display the image
+# draw a bounding box arounded the detected barcode and display the
+# image
 cv2.drawContours(image, [box], -1, (0, 255, 0), 3)
 cv2.imshow("Image", image)
-cv2.waitkey(0)
+cv2.waitKey(0)
